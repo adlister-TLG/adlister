e7b8698c0c04bf5ad17dfcf6312fc8f7f86e49e5
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-12-20 10:59:43 -0600

    updated upload servlet, added dependencies for file upload to maven (pom.xml),

--- src/main/java/com/codeup/adlister/controllers/UploadServlet.java
+++ src/main/java/com/codeup/adlister/controllers/UploadServlet.java
@@ -1,18 +1,27 @@
 package com.codeup.adlister.controllers;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 
 /**
  * Created by lloyd on 12/20/16.
  */
 @WebServlet(name = "UploadServlet")
+@MultipartConfig
 public class UploadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String description = request.getParameter("description");
+        Part filepart = request.getPart("file");
+        String fileName = Paths.get(filepart.getSubmittedFileName()).getFileName().toString();
+        InputStream fileContent = filepart.getInputStream();
 
     }
 
