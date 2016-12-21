7e6c52ab01632c15046cdffa7f332d88ffbc0b08
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-12-21 11:47:15 -0600

    Created Image Upload Servlet

---
+++ src/main/java/com/codeup/adlister/controllers/UploadImageServlet.java
@@ -0,0 +1,22 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by lloyd on 12/21/16.
+ */
+@WebServlet(name = "UploadImageServlet")
+public class UploadImageServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
